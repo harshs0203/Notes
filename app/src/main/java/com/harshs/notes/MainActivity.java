481da0d1a63ea1f4601dedc41462f82855afe6c4
harsh0203 <harshs0203@gmail.com>
2020-05-13 22:53:32 +0530

    Setting up list basics

--- app/src/main/java/com/harshs/notes/MainActivity.java
+++ app/src/main/java/com/harshs/notes/MainActivity.java
@@ -1,16 +1,50 @@
 package com.harshs.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()){
+            case R.id.add:
+
+                return  true;
+        }
+
+        return false;
+    }
+
+    ArrayList<String> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView =  findViewById(R.id.listView);
+        notes.add("Example Note");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
